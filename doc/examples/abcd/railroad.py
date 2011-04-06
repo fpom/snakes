a1495da1588dc57249b519db35f530155b80490d
Franck Pommereau <franck.pommereau@gmail.com>
2011-04-06 16:26:23 +0200

    copied missing files

--- doc/examples/abcd/railroad.py
+++ doc/examples/abcd/railroad.py
@@ -1,11 +1,15 @@
-from snakes.nets import *
+import snakes.plugins
+snakes.plugins.load("gv", "snakes.nets", "snk")
+from snk import *
 
 n = loads(",railroad.pnml")
 g = StateGraph(n)
 for s in g :
     m = g.net.get_marking()
+    # safety property: train present => gates closed
     if ("train().crossing" in m
         and True in m["train().crossing"]
         and "closed" not in m["gate().state"]) :
         print s, m
 print "checked", len(g), "states"
+g.draw(",rr-states.png")
