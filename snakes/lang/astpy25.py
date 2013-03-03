3acaca452e8a68842f4f61305df870b195767dce
Franck Pommereau <franck.pommereau@gmail.com>
2013-03-03 09:40:12 +0100

    doc converted

--- snakes/lang/astpy25.py
+++ snakes/lang/astpy25.py
@@ -1,7 +1,7 @@
-"""Backport of Python 2.6 'ast' module.
+"""Backport of Python 2.6 `ast` module.
 
-AST classes are wrappers around classes in Python 2.5 '_ast' module.
-The rest is copied from Python 2.6 'ast.py'.
+AST classes are wrappers around classes in Python 2.5 `_ast` module.
+The rest is copied from Python 2.6 `ast.py`.
 """
 
 import inspect
@@ -33,11 +33,10 @@ for name, cls in inspect.getmembers(_ast, inspect.isclass) :
 del _Ast, cls, name
 
 def literal_eval(node_or_string):
-    """
-    Safely evaluate an expression node or a string containing a Python
-    expression.  The string or node provided may only consist of the following
-    Python literal structures: strings, numbers, tuples, lists, dicts, booleans,
-    and None.
+    """Safely evaluate an expression node or a string containing a
+    Python expression. The string or node provided may only consist of
+    the following Python literal structures: strings, numbers, tuples,
+    lists, dicts, booleans, and `None`.
     """
     _safe_names = {'None': None, 'True': True, 'False': False}
     if isinstance(node_or_string, basestring):
@@ -63,13 +62,13 @@ def literal_eval(node_or_string):
     return _convert(node_or_string)
 
 def dump(node, annotate_fields=True, include_attributes=False):
-    """
-    Return a formatted dump of the tree in *node*.  This is mainly useful for
-    debugging purposes.  The returned string will show the names and the values
-    for fields.  This makes the code impossible to evaluate, so if evaluation is
-    wanted *annotate_fields* must be set to False.  Attributes such as line
-    numbers and column offsets are not dumped by default.  If this is wanted,
-    *include_attributes* can be set to True.
+    """Return a formatted dump of the tree in `node`. This is mainly
+    useful for debugging purposes. The returned string will show the
+    names and the values for fields. This makes the code impossible to
+    evaluate, so if evaluation is wanted `annotate_fields` must be set
+    to `False`. Attributes such as line numbers and column offsets are
+    not dumped by default. If this is wanted, `include_attributes` can
+    be set to `True`.
     """
     def _format(node):
         if isinstance(node, AST):
@@ -92,9 +91,8 @@ def dump(node, annotate_fields=True, include_attributes=False):
     return _format(node)
 
 def copy_location(new_node, old_node):
-    """
-    Copy source location (`lineno` and `col_offset` attributes) from
-    *old_node* to *new_node* if possible, and return *new_node*.
+    """Copy source location (`lineno` and `col_offset` attributes)
+    from `old_node` to `new_node` if possible, and return `new_node`.
     """
     for attr in 'lineno', 'col_offset':
         if attr in old_node._attributes and attr in new_node._attributes \
@@ -103,12 +101,12 @@ def copy_location(new_node, old_node):
     return new_node
 
 def fix_missing_locations(node):
-    """
-    When you compile a node tree with compile(), the compiler expects lineno and
-    col_offset attributes for every node that supports them.  This is rather
-    tedious to fill in for generated nodes, so this helper adds these attributes
-    recursively where not already set, by setting them to the values of the
-    parent node.  It works recursively starting at *node*.
+    """When you compile a node tree with `compile()`, the compiler
+    expects `lineno` and `col_offset` attributes for every node that
+    supports them. This is rather tedious to fill in for generated
+    nodes, so this helper adds these attributes recursively where not
+    already set, by setting them to the values of the parent node. It
+    works recursively starting at `node`.
     """
     def _fix(node, lineno, col_offset):
         if 'lineno' in node._attributes:
@@ -127,10 +125,10 @@ def fix_missing_locations(node):
     return node
 
 def increment_lineno(node, n=1):
-    """
-    Increment the line number of each node in the tree starting at *node* by *n*.
-    This is useful to "move code" to a different location in a file.
-    """
+    '''Increment the line number of each node in the tree starting at
+    `node` by `n`. This is useful to "move code" to a different
+    location in a file.
+    '''
     if 'lineno' in node._attributes:
         node.lineno = getattr(node, 'lineno', 0) + n
     for child in walk(node):
@@ -139,9 +137,8 @@ def increment_lineno(node, n=1):
     return node
 
 def iter_fields(node):
-    """
-    Yield a tuple of ``(fieldname, value)`` for each field in ``node._fields``
-    that is present on *node*.
+    """Yield a tuple of `(fieldname, value)` for each field in
+    `node._fields` that is present on `node`.
     """
     for field in node._fields:
         try:
@@ -150,9 +147,8 @@ def iter_fields(node):
             pass
 
 def iter_child_nodes(node):
-    """
-    Yield all direct child nodes of *node*, that is, all fields that are nodes
-    and all items of fields that are lists of nodes.
+    """Yield all direct child nodes of `node`, that is, all fields
+    that are nodes and all items of fields that are lists of nodes.
     """
     for name, field in iter_fields(node):
         if isinstance(field, AST):
@@ -163,10 +159,9 @@ def iter_child_nodes(node):
                     yield item
 
 def get_docstring(node, clean=True):
-    """
-    Return the docstring for the given node or None if no docstring can
-    be found.  If the node provided does not have docstrings a TypeError
-    will be raised.
+    """Return the docstring for the given `node` or `None` if no
+    docstring can be found. If the node provided does not have
+    docstrings a `TypeError` will be raised.
     """
     if not isinstance(node, (FunctionDef, ClassDef, Module)):
         raise TypeError("%r can't have docstrings" % node.__class__.__name__)
@@ -177,10 +172,9 @@ def get_docstring(node, clean=True):
         return node.body[0].value.s
 
 def walk(node):
-    """
-    Recursively yield all child nodes of *node*, in no specified order.  This is
-    useful if you only want to modify nodes in place and don't care about the
-    context.
+    """Recursively yield all child nodes of `node`, in no specified
+    order. This is useful if you only want to modify nodes in place
+    and don't care about the context.
     """
     from collections import deque
     todo = deque([node])
@@ -190,23 +184,23 @@ def walk(node):
         yield node
 
 class NodeVisitor(object):
-    """
-    A node visitor base class that walks the abstract syntax tree and calls a
-    visitor function for every node found.  This function may return a value
-    which is forwarded by the `visit` method.
+    """A node visitor base class that walks the abstract syntax tree
+    and calls a visitor function for every node found. This function
+    may return a value which is forwarded by the `visit` method.
 
-    This class is meant to be subclassed, with the subclass adding visitor
-    methods.
+    This class is meant to be subclassed, with the subclass adding
+    visitor methods.
 
-    Per default the visitor functions for the nodes are ``'visit_'`` +
-    class name of the node.  So a `TryFinally` node visit function would
-    be `visit_TryFinally`.  This behavior can be changed by overriding
-    the `visit` method.  If no visitor function exists for a node
-    (return value `None`) the `generic_visit` visitor is used instead.
+    Per default the visitor functions for the nodes are `visit_` +
+    class name of the node. So a `TryFinally` node visit function
+    would be `visit_TryFinally`. This behavior can be changed by
+    overriding the `visit` method. If no visitor function exists for a
+    node (return value `None`) the `generic_visit` visitor is used
+    instead.
 
-    Don't use the `NodeVisitor` if you want to apply changes to nodes during
-    traversing.  For this a special visitor exists (`NodeTransformer`) that
-    allows modifications.
+    Don't use the `NodeVisitor` if you want to apply changes to nodes
+    during traversing. For this a special visitor exists
+    (`NodeTransformer`) that allows modifications.
     """
     def visit(self, node):
         """Visit a node."""
@@ -224,39 +218,39 @@ class NodeVisitor(object):
                 self.visit(value)
 
 class NodeTransformer(NodeVisitor):
-    """
-    A :class:`NodeVisitor` subclass that walks the abstract syntax tree and
-    allows modification of nodes.
+    """A `NodeVisitor` subclass that walks the abstract syntax tree
+    and allows modification of nodes.
 
-    The `NodeTransformer` will walk the AST and use the return value of the
-    visitor methods to replace or remove the old node.  If the return value of
-    the visitor method is ``None``, the node will be removed from its location,
-    otherwise it is replaced with the return value.  The return value may be the
-    original node in which case no replacement takes place.
+    The `NodeTransformer` will walk the AST and use the return value
+    of the visitor methods to replace or remove the old node. If the
+    return value of the visitor method is `None`, the node will be
+    removed from its location, otherwise it is replaced with the
+    return value. The return value may be the original node in which
+    case no replacement takes place.
 
-    Here is an example transformer that rewrites all occurrences of name lookups
-    (``foo``) to ``data['foo']``::
+    Here is an example transformer that rewrites all occurrences of
+    name lookups (`foo`) to `data['foo']`:
 
-       class RewriteName(NodeTransformer):
+        class RewriteName(NodeTransformer):
+            def visit_Name(self, node):
+                return copy_location(Subscript(
+                    value=Name(id='data', ctx=Load()),
+                    slice=Index(value=Str(s=node.id)),
+                    ctx=node.ctx
+                   ), node)
 
-           def visit_Name(self, node):
-               return copy_location(Subscript(
-                   value=Name(id='data', ctx=Load()),
-                   slice=Index(value=Str(s=node.id)),
-                   ctx=node.ctx
-               ), node)
+    Keep in mind that if the node you're operating on has child
+    nodes you must either transform the child nodes yourself or call
+    the `generic_visit` method for the node first.
 
-    Keep in mind that if the node you're operating on has child nodes you must
-    either transform the child nodes yourself or call the :meth:`generic_visit`
-    method for the node first.
+    For nodes that were part of a collection of statements (that
+    applies to all statement nodes), the visitor may also return a
+    list of nodes rather than just a single node.
 
-    For nodes that were part of a collection of statements (that applies to all
-    statement nodes), the visitor may also return a list of nodes rather than
-    just a single node.
+    Usually you use the transformer like this:
 
-    Usually you use the transformer like this::
+        node = YourTransformer().visit(node)
 
-       node = YourTransformer().visit(node)
     """
     def generic_visit(self, node):
         for field, old_value in iter_fields(node):
@@ -303,8 +297,7 @@ def _ast2ast (node) :
     return new
 
 def parse(expr, filename='<unknown>', mode='exec'):
-    """
-    Parse an expression into an AST node.
-    Equivalent to compile(expr, filename, mode, PyCF_ONLY_AST).
+    """Parse an expression into an AST node. Equivalent to
+    `compile(expr, filename, mode, PyCF_ONLY_AST)`.
     """
     return _ast2ast(compile(expr, filename, mode, PyCF_ONLY_AST))
